c1efa714b710d83e4f960452a7d7c2d5f0de847c
Odhran <mahonyodhran@gmail.com>
2022-06-02 16:34:52 +0100

    updated driver to create objects and print values

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Welcome to ATMjava!");
+
+        Account a1 = new Account("Odhran", 100);
+        System.out.println("Welcome: " + a1.getName() + "! \nYour current balance is: " + String.format("%.2f",a1.getBalance()));
     }
 }
