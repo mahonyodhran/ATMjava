24c145c25735c0008a26dcc022ee0e7883434b23
Odhran <mahonyodhran@gmail.com>
2022-06-02 20:23:24 +0100

    extracted methods to Account class - basic non regex validation

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Account {
     String name;
     double balance;
@@ -28,5 +30,43 @@ public class Account {
     @Override
     public String toString() {
         return "Account [name=" + name + ", balance= " + balance + "]";
+    }
+
+    static String createUserName(Scanner input) {
+        String name;
+        boolean valid;
+        valid = false;
+        name = "";
+        while(!valid){
+            try {
+                System.out.print("What is your name? ");
+                name = input.nextLine();
+                if (name.equals("")) {
+                    System.out.println("Please enter a value");
+                } else {
+                    valid = true;
+                }
+            } catch (NullPointerException e) {
+                System.out.println("Please enter a name");
+            }
+        }
+        return name;
+    }
+
+    static double getInitialDeposit(Scanner input) {
+        double deposit;
+        boolean valid;
+        valid = false;
+        deposit = 0;
+        while (!valid) {
+            try {
+                System.out.print("What is your initial deposit? ");
+                deposit = Double.parseDouble(input.nextLine());
+                valid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number");
+            }
+        }
+        return deposit;
     }
 }
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,29 +9,11 @@ public class App {
         double deposit;
 
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("What is your name?");
-        name = "";
-        try {
-            name = input.nextLine();
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-
-        deposit = 0;
-        System.out.println("What is your initial deposit?");
-        try {
-            deposit = Double.parseDouble(input.nextLine());
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
-
+        name = Account.createUserName(input);
+        deposit = Account.getInitialDeposit(input);
         input.close();
 
         Account user = new Account(name, deposit);
         System.out.println(user.toString());
-
-        // Account a1 = new Account("Odhran", 100);
-        // System.out.println("Welcome: " + a1.getName() + "! \nYour current balance is: " + String.format("%.2f",a1.getBalance()));
     }
 }
