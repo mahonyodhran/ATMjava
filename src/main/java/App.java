991653c754daf959796beb7e5d6d534f55bec4a7
Odhran <mahonyodhran@gmail.com>
2022-06-02 19:43:57 +0100

    added basic logic to take in user input and create an object with

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,37 @@
 package main.java;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         System.out.println("Welcome to ATMjava!");
+        String name;
+        double deposit;
 
-        Account a1 = new Account("Odhran", 100);
-        System.out.println("Welcome: " + a1.getName() + "! \nYour current balance is: " + String.format("%.2f",a1.getBalance()));
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("What is your name?");
+        name = "";
+        try {
+            name = input.nextLine();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        deposit = 0;
+        System.out.println("What is your initial deposit?");
+        try {
+            deposit = Double.parseDouble(input.nextLine());
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        input.close();
+
+        Account user = new Account(name, deposit);
+        System.out.println(user.toString());
+
+        // Account a1 = new Account("Odhran", 100);
+        // System.out.println("Welcome: " + a1.getName() + "! \nYour current balance is: " + String.format("%.2f",a1.getBalance()));
     }
 }
