ac7843bdea3096b3db3ce68f43026284600f7755
Odhran <mahonyodhran@gmail.com>
2022-06-03 20:20:34 +0100

    cleanup

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,15 +1,11 @@
 package test.java;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-
 import org.junit.Test;
 
 public class AccountTest {
-   // private final Account account = new Account("Odhran", 100);
 
     @Test
-    public void testGetBalance() {
+    public void testHere() {
 
     }
 }
